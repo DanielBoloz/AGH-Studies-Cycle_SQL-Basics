e63ac4af971c21ab5198976b495a7674b9707dc5
DanielBoloz <155580915+DanielBoloz@users.noreply.github.com>
2024-04-12 16:48:01 +0200

    Update and rename Exercise 3.sql to Exercise.sql

--- Week 3/Exercise.sql
+++ Week 3/Exercise.sql
@@ -10,8 +10,17 @@ FROM surveys AS s
 JOIN species AS sp
 ON s.species_id = sp.species_id;
 
---3. Query joins 4 oclumns from tables "surveys" and "species" based on a "species_id" with usage of "USING" formula.
+--3. Query joins 4 columns from tables "surveys" and "species" based on a "species_id" with usage of "USING" formula.
 SELECT s.record_id, s.species_id, sp.genus, sp.species
 FROM surveys AS s
 JOIN species AS sp
 USING (species_id);
+
+--4. Query joins 4 tables "surveys", "species", "plots".
+SELECT surveys.species_id, species.genus, species.species, plots.plot_id, plots.plot_type
+FROM surveys
+JOIN species
+ON surveys.species_id = species.species_id
+JOIN plots
+ON surveys.plot_id = plots.plot_id
+WHERE surveys.year=2000;
